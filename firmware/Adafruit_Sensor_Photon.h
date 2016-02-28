7e93c7d829c9937f56a8da732799a6eeaf097bb3
Aaron <thecrentist00@gmail.com>
2016-02-28 00:26:32 -0800

    Update Adafruit_Sensor_Photon.h

--- firmware/Adafruit_Sensor_Photon.h
+++ firmware/Adafruit_Sensor_Photon.h
@@ -20,7 +20,14 @@
 #ifndef _ADAFRUIT_SENSOR_H
 #define _ADAFRUIT_SENSOR_H
 
-#include "application.h"
+//#if ARDUINO >= 100
+ //#include "Arduino.h"
+ //#include "Print.h"
+//#else
+ //#include "WProgram.h"
+//#endif
+
+//#include "application.h"
 
 /* Intentionally modeled after sensors.h in the Android API:
  * https://github.com/android/platform_hardware_libhardware/blob/master/include/hardware/sensors.h */
